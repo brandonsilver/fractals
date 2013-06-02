370e3190029b5bb35d39d7bd283469b3db8e44c6
brandonsilver <brandon@brandonsilver.com>
2013-06-02 11:14:19 -0500

    initialize the repository

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an ongoing project in the earliest stages of development. This 
+directory contains an eclipse workspace with the Java source & binary files.
---
+++ src/DrawFractal.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
+
+public class DrawFractal extends JFrame implements ActionListener, MouseListener, MouseMotion {
+
+	public DrawFractal() {
+		// stuff to setup the drawing environment
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
